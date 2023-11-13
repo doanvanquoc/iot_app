11f48d6dd60ffa8b2be5e44688c486040fe330aa
doanvanquoc <vanquoc.dev@gmail.com>
2023-11-13 22:41:21 +0700

    merge main

---
+++ lib/common/widgets/my_button.dart
@@ -0,0 +1,28 @@
+//Dùng button này cho các button trong app, không cần tự custom
+import 'package:flutter/material.dart';
+import 'package:iot_app/common/apps/app_color.dart';
+import 'package:iot_app/common/apps/app_style.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key, required this.text, required this.onTap});
+  final String text;
+  final Function() onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+        decoration: BoxDecoration(
+          color: AppColor.primaryColor,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: AppStyle.buttonText,
+        ),
+      ),
+    );
+  }
+}
